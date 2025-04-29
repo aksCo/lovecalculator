580bd280775929693d3581f29fc1e698ba6d2174
Aks Sreenivasan <aks.auburn@gmail.com>
2025-04-29 10:24:00 +1000

    LoveCalculator in Python, Add Project

---
+++ LoveCalculator.py
@@ -0,0 +1,8 @@
+def calculate_love_score(name1, name2):
+    combined_names = name1.lower() + name2.lower()
+    true_count = sum(combined_names.count(letter) for letter in "true")
+    love_count = sum(combined_names.count(letter) for letter in "love")
+    love_score = int(str(true_count) + str(love_count))
+    print(love_score)
+
+calculate_love_score("Mindy","Benjamin")
